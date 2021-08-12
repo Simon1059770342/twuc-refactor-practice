ae474f890858968b150528fc557ca520c7187e75
Simon1059770342 <simon1059770342@gmail.com>
2021-08-12 14:43:32 +0800

    [Task4]

--- 4/src/main/java/com/twu/refactoring/NumberCruncher.java
+++ 4/src/main/java/com/twu/refactoring/NumberCruncher.java
@@ -1,5 +1,7 @@
 package com.twu.refactoring;
 
+import java.util.Arrays;
+
 public class NumberCruncher {
     private final int[] numbers;
 
@@ -8,34 +10,26 @@ public class NumberCruncher {
     }
 
     public int countEven() {
+        return EvenOrOdd(0);
+    }
+
+    private int EvenOrOdd(int i) {
         int count = 0;
         for (int number : numbers) {
-            if (number % 2 == 0) count++;
+            if (number % 2 == i) count++;
         }
         return count;
     }
 
     public int countOdd() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 1) count++;
-        }
-        return count;
+        return EvenOrOdd(1);
     }
 
     public int countPositive() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number >= 0) count++;
-        }
-        return count;
+        return (int) Arrays.stream(numbers).filter(i -> i >= 0).count();
     }
 
     public int countNegative() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number < 0) count++;
-        }
-        return count;
+        return (int) Arrays.stream(numbers).filter(i -> i < 0).count();
     }
 }
